44b23fec060ba98c4acaf910ce2abd7bd5f0f8c3
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-04-02 15:51:35 +0200

    metoda main: zastapienie znaku nowej lini funkcja println

--- src/PodstawyGitInteliJ.java
+++ src/PodstawyGitInteliJ.java
@@ -1,10 +1,10 @@
 public class PodstawyGitInteliJ {
 
     public static void main(String[] args) {
-        System.out.print("Uczę \n" +
-                "się \n" +
-                "programowania \n" +
-                "i \n" +
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+        System.out.println("i \n" +
                 "commitowania");
     }
 }
